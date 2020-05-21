af8e0e20110a42b9a8cd5964142cde05c67eadec
pasindu_07 <dmpasindu98@gmail.com>
2020-05-21 21:35:42 +0530

    new V

---
+++ Assignment 02/Q2.scala
@@ -0,0 +1,23 @@
+object Q2 extends App{
+
+    def attendees(ticket_price:Int):Int={
+        return 120+ ((15-ticket_price)/5)*20;
+    }
+
+    def cost_per_show(attendees_pershow:Int,ticket_price:Int):Int={
+        return 500 + attendees_pershow*3;
+    }
+
+    def income(attendees_pershow:Int,ticket_price:Int):Int={
+        return attendees_pershow*ticket_price;
+    }
+
+    def profit(ticket_price:Int):Int={
+        return income(attendees(ticket_price),ticket_price)-cost_per_show(attendees(ticket_price),ticket_price);
+    }
+
+    print("Ticket Price:");val ticket_price = scala.io.StdIn.readInt();
+    printf("Profit fot the ticket price Rs.%d = %d\n",ticket_price,profit(ticket_price));
+
+
+}
